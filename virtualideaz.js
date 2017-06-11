93a84567a65f8809b45464daa42d2399a5582117
Marlen Palayad <palayadmarlen@gmail.com>
2017-06-11 16:22:58 +0800

    Delete virtualideaz.js

--- virtualideaz.js
+++
@@ -1,7 +0,0 @@
-//this is the javascript for your transition
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar-fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
